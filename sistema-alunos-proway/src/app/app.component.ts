175265531734c5e0c4e293c7a8b6cf7168bbcfff
Luciano <lucianohostindossantos@gmail.com>
2025-02-12 22:08:04 -0300

    Modificando componente Student

--- sistema-alunos-proway/src/app/app.component.ts
+++ sistema-alunos-proway/src/app/app.component.ts
@@ -8,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sistema Aluno ProWay';
+
+  listAluno = [
+    {
+      img: 'assets/bochi the rock.jpg',
+      nome: 'luciano',
+      curso: 'Angular',
+      matricula: 4311,
+      email: 'luciano@gmail.com',
+      status: true
+
+    },
+    {
+      img: 'assets/bochi1.png',
+      nome: 'alex',
+      curso: 'Typescript',
+      matricula: 222,
+      email: 'alex@gmail.com',
+      status: true
+
+    },
+    {
+      img: 'assets/bochi2.png',
+      nome: 'renan',
+      curso: 'C++',
+      matricula: 777,
+      email: 'renan@gmail.com',
+      status: false
+
+    },
+
+  ];
+
 }
